877748206a2492c78701cbab7308d0cfc3310147
Danillo de Lacerda <danillo.lacerda77@gmail.com>
2024-08-23 22:46:04 -0300

    Fizemos o método do cáluclo do valor da gorjeta

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -10,21 +10,39 @@
 
         private void SetLowTip_Clicked(object sender, EventArgs e)
         {
-
+            TipSlider.Value = 15;
+            TipPercentLabel.Text = TipSlider.Value.ToString() + "%";
         }
 
         private void SetHighTip_Clicked(object sender, EventArgs e)
         {
-
+            TipSlider.Value = 20;
+            TipPercentLabel.Text = TipSlider.Value.ToString() + "%";
         }
 
         private void RoundUpButton_Clicked(object sender, EventArgs e)
         {
-
+            
         }
 
         private void RoundDownButton_Clicked(object sender, EventArgs e)
         {
+            
+        }
+
+        private void TipSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            TipPercentLabel.Text = TipSlider.Value.ToString("#.##") + "%";
+        }
+
+        private void AmountEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            double Valor = Convert.ToDouble(TipSlider.Value.ToString());
+            double ValorDaGorjeta = Valor * (TipSlider.Value/100);
+            double ValorTotal = ValorDaGorjeta + Valor;
+
+            TotalLabel.Text = ValorTotal.ToString("C");
+            TipLabel.Text = ValorDaGorjeta.ToString("C");
 
         }
     }
